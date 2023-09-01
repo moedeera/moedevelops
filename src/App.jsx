b8f22620cf5f7236ea78e69a6d22ce4f4ba6036a
moedeera <82620264+moedeera@users.noreply.github.com>
2023-09-01 04:15:44 -0600

    committ

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./Pages/Home/Home";
+import { About } from "./Pages/About/About";
+import { Login } from "./Pages/Login/Login";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       {" "}
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
