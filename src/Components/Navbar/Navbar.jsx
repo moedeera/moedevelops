6a69d21d102d35310d698f766e72d4310ab3259b
moedeera <82620264+moedeera@users.noreply.github.com>
2023-09-01 08:07:18 -0600

    committ

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,10 +2,25 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 export const Navbar = () => {
   return (
-    <div className="navbar">
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-      <Link to={"/login"}>Login</Link>
-    </div>
+    <nav className="relative container p-6 mx-auto">
+      <div className="flex items-center justify-between">
+        {" "}
+        <div className="flex gap-x-2.5">
+          {" "}
+          <Link to={"/"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/login"}>Portfolio</Link>
+        </div>
+        <div className="">
+          <h3>
+            Moe<span>Develops</span>
+          </h3>
+        </div>
+        <div className="hidden md:flex gap-x-2.5">
+          {" "}
+          <Link to={"/login"}>Login</Link>
+        </div>
+      </div>
+    </nav>
   );
 };
