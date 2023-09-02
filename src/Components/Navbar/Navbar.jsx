f2a366bda8ba371a599ea8d67dd8bdbcc9fe7035
moedeera <82620264+moedeera@users.noreply.github.com>
2023-09-02 16:58:33 -0600

    committ

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,14 @@ import "./Navbar.css";
 import menu from "./navbar.png";
 export const Navbar = () => {
   return (
-    <nav className="relative container p-6 mx-auto text-black">
+    <nav className="relative  p-6 mx-auto text-black w-full">
       <div className="flex items-center justify-between">
         {" "}
         <div className="hidden md:block">
-          <h3>
-            Moe<span>Develops</span>
+          <h3 className="bold">
+            <Link to={"/"}>
+              Moe<span className="text-primary">Develops</span>
+            </Link>
           </h3>
         </div>
         <div className=" md:hidden">
@@ -16,25 +18,25 @@ export const Navbar = () => {
         </div>
         <div className="hidden md:flex gap-x-2.5">
           {" "}
-          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>
+            <p>Portfolio</p>
+          </Link>
+          <Link to={"/about"}>
+            <p className="hover:text-red"> Services</p>
+          </Link>
           <Link to={"/about"}>About</Link>
-          <Link to={"/login"}>Portfolio</Link>
         </div>
         <div className="md:hidden">
           <h3>
-            Moe<span>Develops</span>
+            <Link to={"/"}>
+              Moe<span className="text-primary">Develops</span>
+            </Link>
           </h3>
         </div>
         <div className="hidden md:flex gap-x-2.5">
           {" "}
-          <Link to={"/login"}>
-            <button
-              className="bg-black text-white hover:bg-gray-100 text-black font-semibold py-2 px-4 border border-gray-400 rounded shadow transition-property: all;
-transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-transition-duration: 150ms;"
-            >
-              Button
-            </button>
+          <Link to={"/login"} className="text-red">
+            <button className="btn">Contact</button>
           </Link>
         </div>
       </div>
