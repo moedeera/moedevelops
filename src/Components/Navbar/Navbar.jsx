a8d0587811d5806ea6910c27aa123b10c8de4d20
moedeera <82620264+moedeera@users.noreply.github.com>
2023-09-03 16:10:00 -0600

    committ

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import menu from "./navbar.png";
+import close from "./close.png";
+import { useState } from "react";
 export const Navbar = () => {
+  const [mobileMenu, showMobileMenu] = useState(false);
   return (
-    <nav className="relative  p-6 mx-auto text-black w-full">
+    <nav className="relative  p-6 h-24 mx-auto text-black w-full">
       <div className="flex items-center justify-between">
         {" "}
         <div className="hidden md:block">
@@ -13,8 +16,17 @@ export const Navbar = () => {
             </Link>
           </h1>
         </div>
-        <div className=" md:hidden">
-          <img src={menu} alt="menu" />
+        <div
+          className=" md:hidden cursor-pointer"
+          onClick={() => {
+            showMobileMenu(!mobileMenu);
+          }}
+        >
+          {mobileMenu ? (
+            <img src={close} alt="menu" />
+          ) : (
+            <img src={menu} alt="menu" />
+          )}
         </div>
         <div className="hidden md:flex gap-x-3.5">
           {" "}
@@ -40,12 +52,14 @@ export const Navbar = () => {
           </Link>
         </div>
       </div>
-      <nav className="absolute w-full left-0 top-0 h-full flex flex-col border">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/portfolio"}>Portfolio</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/contact"}>Contact</Link>
-      </nav>
+      {mobileMenu && (
+        <nav className="absolute w-full p-6 left-0 top-24 h-96 flex justify-evenly flex-col border md:hidden">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/portfolio"}>Portfolio</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/contact"}>Contact</Link>
+        </nav>
+      )}
     </nav>
   );
 };
