70efb73dadf5019e33309592e74f7660b3fd3cb0
Moe Deera <deeramoe@gmail.com>
2023-09-05 04:49:14 -0600

    carousel setup

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,3 +1,28 @@
+import "./Carousel.css";
 export const Carousel = () => {
-  return <div>Carousel</div>;
+  const items = [
+    { name: "goldenrod" },
+    { name: "crimson" },
+    { name: "red" },
+    { name: "blue" },
+    { name: "green" },
+    { name: "purple" },
+    { name: "orange" },
+  ];
+
+  return (
+    <div>
+      <div className="carousel">
+        {items.map((item) => (
+          <div
+            key={item.name}
+            className="carousel-item"
+            style={{ backgroundColor: `${item.name}` }}
+          >
+            <h1>{item.name}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { Carousel } from "../../Components/Carousel/Carousel";
 import { Landing } from "../../Components/Landing/Landing";
 
 export const Home = () => {
   return (
     <div className="page-container">
       <Landing />
+      <Carousel />
     </div>
   );
 };
