bf6d5c497830b3bf04cc4b1d7dc55f1f3b40e1ff
moedeera <82620264+moedeera@users.noreply.github.com>
2023-09-14 07:29:52 -0600

    committ

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,3 +1,19 @@
+import "./Portfolio.css";
 export const Portfolio = () => {
-  return <div className="page-container">Portfolio</div>;
+  return (
+    <div className="page-container">
+      Portfolio
+      <div className="portfolio"></div>
+      <div className="portfolio-container">
+        <div className="portfolio-item"></div>
+        <div className="portfolio-item"></div>
+        <div className="portfolio-item"></div>
+        <div className="portfolio-item"></div>
+        <div className="portfolio-item"></div>
+        <div className="portfolio-item"></div>
+        <div className="portfolio-item"></div>
+        <div className="portfolio-item"></div>
+      </div>
+    </div>
+  );
 };
