7b4e86a0c6f55657d2c7c269955e1133b9934f9b
Moe Deera <deeramoe@gmail.com>
2023-09-20 05:58:35 -0600

    contact messages setup

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,19 +1,65 @@
 import "./Form.css";
+import { db } from "../../firebase-config";
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+
 export const Form = () => {
+  // const [sent, setSent] = useState(false);
+  // const messageData = collection(db, "messages");
+  const [msg, setMessage] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const onChangeHandler = (e, type, userInfo, setUserInfo) => {
+    if (type === "name") {
+      setUserInfo({ ...userInfo, name: e.target.value });
+    } else if (type === "email") {
+      setUserInfo({ ...userInfo, email: e.target.value });
+    } else if (type === "message") {
+      setUserInfo({ ...userInfo, password: e.target.value });
+    }
+  };
+
   return (
     <div className="form">
       <div className="input-section">
         <div className="input-label">Name or Company</div>
-        <input type="text" placeholder="Enter your name" />
+        <input
+          name="name"
+          type="name"
+          value={msg.name}
+          onChange={(e) => {
+            onChangeHandler(e, "name", msg, setMessage);
+          }}
+          placeholder="Enter an Name"
+        />
       </div>
       <div className="input-section">
         {" "}
         <div className="input-label">Email</div>
-        <input type="text" placeholder="Enter an Email" />
+        <input
+          name="email"
+          type="email"
+          value={msg.email}
+          onChange={(e) => {
+            onChangeHandler(e, "email", msg, setMessage);
+          }}
+          placeholder="Enter an Email"
+        />
       </div>
       <div className="text-section">
         <div className="input-label">Message</div>
-        <textarea placeholder="Type in message" cols={40} rows={7} />
+        <textarea
+          value={msg.message}
+          onChange={(e) => {
+            onChangeHandler(e, "message", msg, setMessage);
+          }}
+          placeholder="Type in message"
+          cols={40}
+          rows={7}
+        />
       </div>
       <button className="btn">Submit</button>
     </div>
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,16 +6,25 @@ import { getStorage, ref } from "firebase/storage";
 // import { } from 'firebase/<service>';
 
 // TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyACCj3pb7TCbohkdMp0sZt_b8iAahjNVcg",
-  authDomain: "moedevelops.firebaseapp.com",
-  projectId: "moedevelops",
-  storageBucket: "moedevelops.appspot.com",
-  messagingSenderId: "319955006674",
-  appId: "1:319955006674:web:21f1cb783cd71fe809290b",
-  measurementId: "G-2S8LN0R0BT",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyACCj3pb7TCbohkdMp0sZt_b8iAahjNVcg",
+//   authDomain: "moedevelops.firebaseapp.com",
+//   projectId: "moedevelops",
+//   storageBucket: "moedevelops.appspot.com",
+//   messagingSenderId: "319955006674",
+//   appId: "1:319955006674:web:21f1cb783cd71fe809290b",
+//   measurementId: "G-2S8LN0R0BT",
+// };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCH49nJNK0-SinR4VT6V0a-_FZsapLgtJ8",
+  authDomain: "portfolio-33f0f.firebaseapp.com",
+  projectId: "portfolio-33f0f",
+  storageBucket: "portfolio-33f0f.appspot.com",
+  messagingSenderId: "170316975789",
+  appId: "1:170316975789:web:f8c5aaff8e7199f7920024",
+  measurementId: "G-M0ZQL4395P",
+};
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
