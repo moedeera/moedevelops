2fd3d91ce366708c8030c3e8081858d236299207
Moe Deera <deeramoe@gmail.com>
2023-09-20 05:59:17 -0600

    errors fixed

--- src/Pages/Content/Content.jsx
+++ src/Pages/Content/Content.jsx
@@ -2,9 +2,18 @@ import { useContext } from "react";
 import "./Content.css";
 import { SiteContext } from "../../Context/Context";
 import { LoginForm } from "./LoginForm";
+// import { collection, addDoc } from "firebase/firestore";
+// import { db } from "../../config/firebase";
 
 export const Content = () => {
   const { user, logState } = useContext(SiteContext);
+  const [sent, setSent] = useState(false);
+  // const messageData = collection(db, "messages");
+  const [msg, setMessage] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
   console.log(user);
 
