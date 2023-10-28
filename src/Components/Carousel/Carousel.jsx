666eb29737daa0f7ac24df01e0a2220364b04abb
Moe Deera <deeramoe@gmail.com>
2023-10-28 04:00:57 -0600

    Homepage fix up

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -3,10 +3,46 @@ import { findImageSet, imagesSorted } from "../../assets/Portfolio/images";
 import { projectList } from "../../assets/Portfolio/projects";
 
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 export const Carousel = () => {
+  const componentRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1, // Intersection threshold for 30% visibility
+    };
+
+    const intersectionCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          setHasAnimated(true); // Set the flag to prevent future animations
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(intersectionCallback, options);
+    observer.observe(componentRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [hasAnimated]);
+
   return (
-    <div>
+    <div
+      style={{
+        opacity: isVisible ? 1 : 0,
+
+        transition: "opacity 1.5s",
+      }}
+      ref={componentRef}
+    >
       <div className={`carousel slide-left`}>
         {projectList.map((item) => (
           <Link
--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,8 +1,44 @@
 import { Link } from "react-router-dom";
 import "./Landing.css";
+import { useEffect, useRef, useState } from "react";
 export const Landing = () => {
+  const componentRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3, // Intersection threshold for 30% visibility
+    };
+
+    const intersectionCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          setHasAnimated(true); // Set the flag to prevent future animations
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(intersectionCallback, options);
+    observer.observe(componentRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [hasAnimated]);
   return (
-    <div className="landing ">
+    <div
+      className="landing "
+      style={{
+        opacity: isVisible ? 1 : 0,
+        transform: isVisible ? "translateX(0)" : "translateY(10%)",
+        transition: "opacity 0.75s, transform 1s",
+      }}
+      ref={componentRef}
+    >
       <div>
         {" "}
         <small>Build Quality Websites</small>
